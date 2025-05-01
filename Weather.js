b75fa8c6030d84e7c0c04ea271c92b2ffbd5dc8b
Bhabana dangi <bhabanadangi42@gmail.com>
2025-05-01 22:33:55 +0545

    Add weather app HTML, CSS, and JavaScript files

---
+++ Weather.js
@@ -0,0 +1,52 @@
+const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWheather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "*C";
+    document.querySelector(".humidity").innerHTML = data.wind.speed + " km/h";
+
+    console.log(data.weather[0].main);
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "img/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "img/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "img/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "img/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "img/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWheather(searchBox.value);
+});
+
+checkWheather();
+forecast: {
+    forecastday: [
+        { date: "2025-08-05", day: { avgtemp_c: 30, condition: { icon: "//cdn.weatherapi.com/weather/64x64/day/116.png" } } },
+        { date: "2025-08-06", day: { avgtemp_c: 30, condition: { icon: "//cdn.weatherapi.com/weather/64x64/day/113.png" } } },
+        { date: "2025-08-07", day: { avgtemp_c: 30, condition: { icon: "//cdn.weatherapi.com/weather/64x64/day/122.png" } } },
+        { date: "2025-08-08", day: { avgtemp_c: 30, condition: { icon: "//cdn.weatherapi.com/weather/64x64/day/115.png" } } },
+    ]
+}
